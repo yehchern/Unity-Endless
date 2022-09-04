ce767599dd3e53a21d5f59c373b2853e1f2b433f
michelle4232 <62208026+michelle4232@users.noreply.github.com>
2022-09-04 21:27:01 +0800

    add selectedCharacter

--- Unity Endless Runner/Assets/Scripts/Characters/Player.cs
+++ Unity Endless Runner/Assets/Scripts/Characters/Player.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI; // Text type variable is a UI element
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class CharacterManager : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public CharacterDatabase characterDB;
 
-    public TextMeshProUGUI nameText;
+    //public TextMeshProUGUI nameText;
     public SpriteRenderer artworkSprite;
 
     private int selectedOption = 0;//keep track which character is be in selection
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,38 +25,10 @@ public class CharacterManager : MonoBehaviour
         else
         {
             Load();
-            
-        }
 
-        //UpdateCharacter(selectedOption);
-        
-    }
-
-    //This function will be called when player click the next button
-    public void NextOption()
-    {
-        selectedOption ++;
-
-        if(selectedOption >= characterDB.CharacterCount)
-        {
-            selectedOption = 0;
         }
 
         UpdateCharacter(selectedOption);
-        Save();
-    }
-
-    public void BackOption()
-    {
-        selectedOption--;
-
-        if(selectedOption < 0)
-        {
-            selectedOption = characterDB.CharacterCount - 1;
-        }
-
-        UpdateCharacter(selectedOption);
-        Save();
     }
 
     //Update character sprite and character name
@@ -63,7 +36,7 @@ public class CharacterManager : MonoBehaviour
     {
         Character character = characterDB.GetCharacter(selectedOption);
         artworkSprite.sprite = character.characterSprite;
-        nameText.text = character.characterName; 
+        //nameText.text = character.characterName;
     }
 
     private void Load()
@@ -71,16 +44,5 @@ public class CharacterManager : MonoBehaviour
         selectedOption = PlayerPrefs.GetInt("selectedOption");
         Debug.Log("?????????????????");
         Debug.Log(selectedOption);
-    }
-
-    private void Save()
-    {//save date
-        PlayerPrefs.SetInt("selectedOption", selectedOption);
-        //save variable to the key name
-    }
-    /**/
-    public void ChangeScene(int sceneID)
-    {
-        SceneManager.LoadScene(sceneID);
     }
 }
